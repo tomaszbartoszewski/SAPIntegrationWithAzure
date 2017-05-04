453c12d98e91e1673256372822aa5f3ab35cffc6
tomaszbartoszewski <tomasz.bartoszewski@gmail.com>
2017-05-04 22:55:49 +0100

    Create simple asp.net core CRUD

---
+++ CoreApplication/CoreApplication/Data/CoreApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreApplication.Models
+{
+    public class CoreApplicationContext : DbContext
+    {
+        public CoreApplicationContext (DbContextOptions<CoreApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CoreApplication.Models.Movie> Movie { get; set; }
+    }
+}
