23cc781ecf4ecdd16fa6e683cc03fb1ff69a0d5c
tomaszbartoszewski <tomasz.bartoszewski@gmail.com>
2017-05-07 23:00:20 +0100

    OutboundStudent webjob receiving messages from service bus

---
+++ WebJobs/WebJobs/Functions.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using Microsoft.Azure.WebJobs;
+
+namespace OutboundStudent
+{
+    public class Functions
+    {
+        // This function will get triggered/executed when a new message is written 
+        // on an Azure Queue called queue.
+        public static void ProcessOutboundStudent([ServiceBusTrigger("domainevents", "outbound-students")] StudentChangedEvent message, TextWriter log)
+        {
+            log.WriteLine(message);
+        }
+    }
+
+    public class StudentChangedEvent
+    {
+        public int StudentId { get; set; }
+    }
+}
---
+++ WebJobs/WebJobs/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.ServiceBus;
+using Microsoft.Azure.WebJobs.Host;
+using System.Configuration;
+using Microsoft.Azure;
+
+namespace OutboundStudent
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see https://go.microsoft.com/fwlink/?LinkID=320976
+    public class Program
+    {
+        // Please set the following connection strings in app.config for this WebJob to run:
+        // AzureWebJobsDashboard and AzureWebJobsStorage
+        public static void Main()
+        {
+            var config = new JobHostConfiguration();
+
+            if (config.IsDevelopment)
+            {
+                config.UseDevelopmentSettings();
+            }
+
+            ServiceBusConfiguration serviceBusConfiguration = new ServiceBusConfiguration { ConnectionString = GetSetting("Microsoft.ServiceBus.ConnectionString") };
+            serviceBusConfiguration.MessageOptions.AutoRenewTimeout = TimeSpan.FromMinutes(5);
+
+            config.UseServiceBus(serviceBusConfiguration);
+
+            var host = new JobHost(config);
+            // The following code ensures that the WebJob will be running continuously
+            host.RunAndBlock();
+        }
+
+        public static string GetSetting(string key)
+        {
+#if DEBUG
+            var value = ConfigurationManager.AppSettings.Get(key);
+#else
+            var value = CloudConfigurationManager.GetSetting(key);
+#endif
+            if (value == null) throw new ConfigurationErrorsException($"No value configured for key: '{key}'");
+            return value;
+        }
+    }
+}
